260bad91b14a21607aa2ddc3103e21ef2a88226e
cleitoncmf <37081944+cleitoncmf@users.noreply.github.com>
2020-08-12 20:58:25 -0300

    Alguns testes com HLS

---
+++ Testes-CMF-12-08-2020/VIVADO-testes/project_Testes/project_Testes.srcs/sources_1/bd/design_Teste_cmf_soma_int/hdl/design_Teste_cmf_soma_int_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+//Date        : Wed Aug 12 15:11:38 2020
+//Host        : DESKTOP-5MP17PP running 64-bit major release  (build 9200)
+//Command     : generate_target design_Teste_cmf_soma_int_wrapper.bd
+//Design      : design_Teste_cmf_soma_int_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_Teste_cmf_soma_int_wrapper
+   ();
+
+
+  design_Teste_cmf_soma_int design_Teste_cmf_soma_int_i
+       ();
+endmodule
