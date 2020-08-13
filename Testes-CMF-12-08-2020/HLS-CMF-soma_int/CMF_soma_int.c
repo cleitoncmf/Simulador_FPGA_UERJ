71f57f7b49a39e1e1a3b7422130ae830646e7829
cleitoncmf <37081944+cleitoncmf@users.noreply.github.com>
2020-08-12 22:21:02 -0300

    projetos HLS

---
+++ Testes-CMF-12-08-2020/HLS-CMF-soma_int/CMF_soma_int.c
@@ -0,0 +1,9 @@
+#include "CMF_soma_int.h"
+
+
+data_t CMF_soma_int(data_t Entrada1, data_t Entrada2){
+
+
+    return Entrada1 + Entrada2;
+
+}
---
+++ Testes-CMF-12-08-2020/HLS-CMF_testa_array_2/CMF_teste_array_2/solution1/sdx.tcl
@@ -0,0 +1,11 @@
+# ==============================================================
+# File generated on Wed Aug 12 20:30:45 -0300 2020
+# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2018.3 (64-bit)
+# SW Build 2405991 on Thu Dec  6 23:38:27 MST 2018
+# IP Build 2404404 on Fri Dec  7 01:43:56 MST 2018
+# Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+# ==============================================================
+add_files CMF_teste_array_2.h
+add_files CMF_teste_array_2.c
+set_part xa7a12tcsg325-1q
+create_clock -name default -period 10
